91840e3a22272fbc65857491bbb9a5e6def64fd7
Andre Viallon <viallon.andre@gmail.com>
2021-03-31 11:18:54 +0200

    setup styled components and typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 50px;
+    color: black;
+`;
+
+export default function Home() {
+  return (
+    <Title>Invoice App</Title>
+  )
+}
