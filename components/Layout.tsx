6916c480a5c17761875b33c51af96bf82a194c57
Andre Viallon <viallon.andre@gmail.com>
2021-04-10 16:49:43 +0200

    add narbar to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,13 @@
+import Narbar from './Narbar';
+import { ThemeEnum } from '../models/Theme';
+
 export default function Layout({ children }) {
     return (
-        <div>
-            {children}
+        <div className="flex flex-col sm:flex-row">
+            <Narbar theme={ThemeEnum.LIGHT} />
+            <div className="w-full h-full">
+                {children}
+            </div>
         </div>
     );
 }
--- components/Narbar.tsx
+++ components/Narbar.tsx
@@ -1,5 +1,4 @@
-import logo from '../public/logo_with_background.svg';
-import avatar from '../public/avatar.png';
+import Image from 'next/image';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ThemeEnum } from '../models/Theme';
@@ -16,27 +15,35 @@ const Narbar:React.FC<Props> = ({ theme }) => {
         flex
         flex-row
         justify-between
-        rounded-2xl
-        rounded-t-none
-
+        w-24
+      
         sm:flex-col
         sm:items-center
         sm:h-screen
-        sm:w-24
-        sm:rounded-l-none
-        sm:rounded-t-2xl
-    `;
+        `;
 
     const iconStyles = `text-secondary-dark text-xl hover:text-secondary-light`;
 
     return (
-        <div className={narbarStyles}>
-            <img className="cursor-pointer rounded-2xl rounded-l-none w-16 sm:w-full" src={logo} alt="logo" />
+        <div className={`${narbarStyles} narbar`}>
+            <div className="cursor-pointer w-16 sm:w-full">
+                <div className="hidden sm:block">
+                    <Image src="/logo_with_background.svg" alt="Logo" layout="fixed" width={85} height={85} />
+                </div>
+                <div className="block sm:hidden">
+                    <Image src="/logo_with_background.svg" alt="Logo" layout="fixed" width={64} height={64} />
+                </div>
+            </div>
             <div className="flex justify-end items-center w-full sm:flex-col flex-row pb-0 pr-4 sm:pb-4 sm:pr-0">
                 <div className="cursor-pointer p-6">
                     {theme === ThemeEnum.LIGHT ? <FontAwesomeIcon className={iconStyles} icon={faMoon} /> : <FontAwesomeIcon className={iconStyles} icon={faSun} />}
                 </div>
-                <img className=" w-10 h-10 sm:w-12 sm:h-12" src={avatar} alt="avatar" />
+                <div className="hidden sm:block">
+                    <Image src="/avatar.png" alt="Avatar" layout="fixed" width={64} height={64} />
+                </div>
+                <div className="block sm:hidden">
+                    <Image src="/avatar.png" alt="Avatar" layout="fixed" width={45} height={45} />
+                </div>
             </div>
         </div>
     )
