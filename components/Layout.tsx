4eea303ac80b024ac19923ded35496d39aefee88
Andre Viallon <viallon.andre@gmail.com>
2021-04-12 17:04:27 +0200

    remove unused import

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,11 @@
 import Narbar from './Narbar';
-import { ThemeEnum } from '../models/Theme';
 
 export default function Layout({ children }) {
     return (
         <div className="flex flex-col sm:flex-row">
             <Narbar />
             <div className="w-full h-full">
-                <div className="container mx-auto mt-6 p-4">
+                <div className="container mx-auto my-10 p-4">
                     {children}
                 </div>
             </div>
