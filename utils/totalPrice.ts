b91bb48f2875ab98cc48cd6cb9c97d9804c86eae
Andre Viallon <viallon.andre@gmail.com>
2021-04-13 18:03:17 +0200

    Refactor total price util

--- utils/totalPrice.ts
+++ utils/totalPrice.ts
@@ -4,7 +4,7 @@ const totalPrice = (items: Item[]): string => {
     let totalPrice;
 
     for (let item of items) {
-        totalPrice =+ item.total;
+        totalPrice = totalPrice ? totalPrice + item.quantity * item.price : item.quantity * item.price;
     }
 
     return totalPrice.toString();
