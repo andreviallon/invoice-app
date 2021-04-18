788635d3bfa5f8411a0bd984902ff1b68c327aa3
Andre Viallon <viallon.andre@gmail.com>
2021-04-18 19:56:08 +0200

    Wrap _app with Layout comp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
 import { ThemeProvider } from 'next-themes';
+import Layout from 'components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   )
 }
