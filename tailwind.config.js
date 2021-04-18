f5911f123940db39164dd4a08381cb9632228087
Andre Viallon <viallon.andre@gmail.com>
2021-04-18 20:02:30 +0200

    change tailwind sm screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       'jumbotron': '7.5rem'
     },
     screens: {
-      'sm': '640px',
+      'sm': '740px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
