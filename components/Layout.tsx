8169a75c66b6520c69cd300ea26891d03d9da83d
Andre Viallon <viallon.andre@gmail.com>
2021-04-21 17:24:11 +0200

    Add extra padding to main container

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
         <div className="flex flex-col sm:flex-row">
             <Navbar />
             <div className="w-full h-full ml-0 sm:ml-20">
-                <div className="container mx-auto mt-16 sm:my-10 p-4">
+                <div className="container mx-auto mt-16 sm:my-10 py-4 px-8">
                     {children}
                 </div>
             </div>
