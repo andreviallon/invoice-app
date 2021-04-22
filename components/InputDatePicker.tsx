c25819b3e321a930ef322ef117c102a158409bd2
Andre Viallon <viallon.andre@gmail.com>
2021-04-22 19:42:29 +0200

    Update datepicker to use standard html input date

--- components/InputDatePicker.tsx
+++ components/InputDatePicker.tsx
@@ -1,23 +1,32 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 
 interface Props {
     label: string;
-    selectedDate?: Date;
-    onDateChange: (date: Date) => void;
+    name: string;
+    selectedDate?: string | Date;
+    error: string;
+    touched: boolean;
+    onDateChange: (date: ChangeEvent) => void;
+
 }
 
-const InputDatePicker: React.FC<Props> = ({ label, selectedDate, onDateChange }) => {
-    const [date, setDate] = useState(selectedDate ? selectedDate : new Date());
+const InputDatePicker: React.FC<Props> = ({ label, name, selectedDate, error, touched, onDateChange }) => {
+    const [date, setDate] = useState(selectedDate);
 
     useEffect(() => {
-        onDateChange(date);
+        setDate(date);
     }, [date]);
+
+    const handleChange = (date) => {
+        console.log('date', date);
+        onDateChange(date.toString());
+    }
 
     return (
         <div className="flex flex-col relative">
-            <label className="text-secondary-dark text-xs font-medium mb-2 dark:text-secondary-light">{label}</label>
-            <DatePicker selected={date} onChange={date => setDate(date)} />
+            <label className="text-secondary-dark text-xs font-medium mb-2 dark:text-secondary-light" htmlFor={name}>{label}</label>
+            <input type="date" name={name} id={name} onChange={date => onDateChange(date)} />
          </div>
     );
 }
