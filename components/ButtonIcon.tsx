c22b57ec3dacc0598794102122e46a5aa4aca906
Andre Viallon <viallon.andre@gmail.com>
2021-04-28 16:11:45 +0200

    Fix button icon colors

--- components/ButtonIcon.tsx
+++ components/ButtonIcon.tsx
@@ -11,11 +11,11 @@ interface Props {
 
 const ButtonIcon: React.FC<Props> = ({ text, buttonType, icon, buttonClick }) => {
     const classes = (buttonType: ButtonIconTypeEnum): string => {
-        let classes: string = 'flex items-center py-1 px-3 rounded-full font-bold text-xs focus:outline-none pr-4 h-12';
+        let classes: string = 'flex items-center py-1 px-3 rounded-full font-bold text-xs focus:outline-none pr-4 h-12 w-full justify-center';
         
         if (buttonType === ButtonIconTypeEnum.PRIMARY) classes = `${classes} text-white bg-primary-regular hover:bg-primary-light`;
 
-        if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = ` ${classes} text-secondary-dark bg-secondary-light hover:bg-secondary-light`;
+        if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = `${classes} text-secondary-dark bg-background hover:bg-secondary-light`;
 
         return classes;
     }
@@ -25,7 +25,7 @@ const ButtonIcon: React.FC<Props> = ({ text, buttonType, icon, buttonClick }) =>
 
         if (buttonType === ButtonIconTypeEnum.PRIMARY) classes = `${classes} text-primary-regular bg-white`;
 
-        if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = ` ${classes} bg-secondary-light hover:bg-secondary-light`;
+        if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = ` ${classes} bg-secondary-veryLight hover:bg-secondary-light`;
 
         return classes;
     }
