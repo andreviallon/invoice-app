2f8dad164d134162ef207e9963baf664d41c66d8
Andre Viallon <viallon.andre@gmail.com>
2021-04-28 16:12:10 +0200

    WIP: move new invoice form to index page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const { theme, setTheme } = useTheme();
 
     const navbarStyles = `
-        bg-gray w-full h-16 flex flex-row justify-between w-24 fixed
+        bg-gray w-full h-16 flex flex-row justify-between w-24 fixed rounded-b-2xl sm:rounded-r-2xl sm:rounded-l-none
       
         sm:flex-col sm:items-center sm:h-screen
 
@@ -23,10 +23,10 @@ const Navbar = () => {
         <div className={`${navbarStyles} navbar`}>
             <div className="cursor-pointer w-16 sm:w-full">
                 <div className="hidden sm:block">
-                    <Image src="/logo_with_background.svg" alt="Logo" layout="fixed" width={85} height={85} />
+                    <Image src="/logo_with_background.svg" className="rounded-r-2xl" alt="Logo" layout="fixed" width={85} height={85} />
                 </div>
                 <div className="block sm:hidden">
-                    <Image src="/logo_with_background.svg" alt="Logo" layout="fixed" width={64} height={64} />
+                    <Image src="/logo_with_background.svg" className="rounded-b-2xl" alt="Logo" layout="fixed" width={64} height={64} />
                 </div>
             </div>
             <div className="flex justify-end items-center w-full sm:flex-col flex-row pb-0 pr-4 sm:pb-4 sm:pr-0">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import InvoiceCard from 'components/InvoiceCard';
 import ButtonIcon from '../components/ButtonIcon';
 import ErrorMessage from '../components/ErrorMessage';
@@ -5,13 +6,14 @@ import connectToDatabase from 'utils/connectToDatabase';
 import { ButtonIconTypeEnum } from 'models/ButtonTypes';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { InvoiceType } from 'models/InvoiceTypes';
-import Link from 'next/link';
+import InvoiceForm from 'components/InvoiceForm';
 
 interface Props {
   invoices: InvoiceType[];
 }
 
 const Home: React.FC<Props> = ({ invoices }) => {
+  const [showModal, setShowModal] = useState(false)
 
   const numberOfInvoices = (numOfInvoices: number): string => {
     if (numOfInvoices === 0) {
@@ -19,23 +21,32 @@ const Home: React.FC<Props> = ({ invoices }) => {
     } else if (numOfInvoices === 1) {
       return 'There is 1 invoice';
     } else {
-      return `There are ${numOfInvoices} invoices`
+      return `There are ${numOfInvoices} invoices`;
     }
-  }
+  };
+
+  const handleInvoice = (invoice: InvoiceType): void => {
+    console.log('submitted invoice', invoice);
+  };
 
   return (
     <>
+      {showModal && (
+        <div className="modal-window-container">
+          <div className="bg-primary-dark bg-opacity-50 absolute right-0 left-0 top-0 bottom-0 z-10" onClick={() => setShowModal(false)}></div>
+          <div className="bg-white dark:bg-black w-4/6 z-20 p-12 absolute h-screen overflow-auto rounded-r-2xl">
+            <InvoiceForm submitInvoice={(invoice: InvoiceType) => handleInvoice(invoice)}/>
+          </div>
+        </div>
+      )}
+
       <div className="flex justify-between items-center mb-12">
         <div>
           <h1 className="text-h1 font-bold dark:text-white">Invoices</h1>
           <p className="dark:text-white text-sm mt-2">{numberOfInvoices(invoices.length)}</p>
         </div>
         <div>
-          <Link href='newInvoice'>
-            <a>
-              <ButtonIcon text="New Invoice" buttonType={ButtonIconTypeEnum.PRIMARY} icon={faPlus} />
-            </a>
-          </Link>
+          <ButtonIcon text="New Invoice" buttonType={ButtonIconTypeEnum.PRIMARY} icon={faPlus} buttonClick={() => setShowModal(true)} />
         </div>
       </div>
 
