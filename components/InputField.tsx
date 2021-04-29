24d19f8dc8205c77d72cf0970aa95ee2b09656c3
Andre Viallon <viallon.andre@gmail.com>
2021-04-29 17:07:37 +0200

    WIP format input field

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent } from 'react';
+import React, { ChangeEvent } from 'react';
 import InputError from './InputError';
+import { Field, ErrorMessage } from 'formik';
 
 interface Props {
     label: string;
     name: string;
-    value?: string;
+    value?: string | number;
     placeholder?: string;
     error: string;
     touched?: boolean;
@@ -22,27 +23,18 @@ const InputField: React.FC<Props> = ({ label, name, value, placeholder, error, t
     }
 
     const inputClasses = `
-        border-secondary-light
-        border
-        rounded
-        px-4
-        py-6
-        text-xs
-        font-bold
-        text-secondary-veryDark
-
-        dark:border-primary-dark
-        dark:bg-primary-dark
-        dark:text-white
-        
-        focus:border-primary-regular
-        focus:outline-none`;
+        border-secondary-light border rounded px-4 py-6 text-xs font-bold text-secondary-veryDark
+        dark:border-primary-dark dark:bg-primary-dark dark:text-white
+        focus:border-primary-regular focus:outline-none`
+    ;
 
     return (
         <div className={containerClasses()}>
             <label className="text-secondary-dark text-xs font-medium mb-2 dark:text-secondary-light">{label}</label>
-            <input name={name} className={inputClasses} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} />
-            {error && touched && <InputError errorMessage={error} />}
+            <Field name={name} className={inputClasses} />
+            {/* <ErrorMessage name={`tickets.${i}.email`} component="div" className="flex flex-col error" /> */}
+            {/* <input name={name} className={inputClasses} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} /> */}
+            {/* {error && touched && <InputError errorMessage={error} />} */}
         </div>
     )
 }
