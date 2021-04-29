bc833b3bd5b2870b50429eadabd9ee8e8a75734d
Andre Viallon <viallon.andre@gmail.com>
2021-04-29 17:07:21 +0200

    formatting

--- components/ButtonIcon.tsx
+++ components/ButtonIcon.tsx
@@ -12,26 +12,20 @@ interface Props {
 const ButtonIcon: React.FC<Props> = ({ text, buttonType, icon, buttonClick }) => {
     const classes = (buttonType: ButtonIconTypeEnum): string => {
         let classes: string = 'flex items-center py-1 px-3 rounded-full font-bold text-xs focus:outline-none pr-4 h-12 w-full justify-center';
-        
         if (buttonType === ButtonIconTypeEnum.PRIMARY) classes = `${classes} text-white bg-primary-regular hover:bg-primary-light`;
-
         if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = `${classes} text-secondary-dark bg-background hover:bg-secondary-light`;
-
         return classes;
     }
 
     const buttonIcon = (buttonType): string => {
         let classes = 'flex h-7 w-7 p-2 mr-4 rounded-full font-bold text-sm items-center';
-
         if (buttonType === ButtonIconTypeEnum.PRIMARY) classes = `${classes} text-primary-regular bg-white`;
-
         if (buttonType === ButtonIconTypeEnum.SECONDARY) classes = ` ${classes} bg-secondary-veryLight hover:bg-secondary-light`;
-
         return classes;
     }
     
     return (
-        <button className={classes(buttonType)} onClick={buttonClick}>
+        <button type="button" className={classes(buttonType)} onClick={buttonClick}>
             <span className={buttonIcon(buttonType)}>
                 <FontAwesomeIcon icon={icon} />
             </span>
