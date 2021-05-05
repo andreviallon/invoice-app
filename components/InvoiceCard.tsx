dcd27ae796364c085680ca14d4fbe9b21d024392
Andre Viallon <viallon.andre@gmail.com>
2021-05-05 17:50:44 +0200

    Use css grid for invoice card

--- components/InvoiceCard.tsx
+++ components/InvoiceCard.tsx
@@ -13,7 +13,7 @@ interface Props {
 const InvoiceCard: React.FC<Props> = ({ invoice }) => {
     const cardClasses = `
         grid grid-cols-2 gap-4 p-4 border-2 bg-white flex-col shadow-lg rounded-lg border-white cursor-pointer
-        sm:p-8 sm:flex sm:flex-row sm:items-center sm:justify-between
+        sm:p-8 sm:grid-cols-11
         hover:border-primary-regular
 
         dark:border-primary-dark dark:bg-primary-dark
@@ -24,14 +24,16 @@ const InvoiceCard: React.FC<Props> = ({ invoice }) => {
         <Link key={invoice._id} href={`/${invoice._id}`}>
           <a>
             <div className={cardClasses}>
-                <span className="text-black text-sm font-bold text-center dark:text-white">{invoice.projectDescription}</span>
-                <span className="text-secondary-dark text-sm font-medium text-center dark:text-white">Due <Moment format="DD MMM YYYY" date={invoice.invoiceDate} /></span>
-                <span className="text-secondary-dark text-sm font-medium text-center dark:text-white">{invoice.client.name}</span>
-                <span className="text-secondary-dark text-sm font-medium text-center dark:text-white">DKK {totalPrice(invoice.itemList)}</span>
-                <div className="col-span-2">
+                <span className="flex items-center col-span-2 text-black text-sm font-bold text-center dark:text-white">{invoice.projectDescription}</span>
+                <span className="flex items-center col-span-2 text-secondary-dark text-sm font-medium text-center dark:text-white">Due <Moment format="DD MMM YYYY" date={invoice.invoiceDate} /></span>
+                <span className="flex items-center col-span-2 text-secondary-dark text-sm font-medium text-center dark:text-white">{invoice.client.name}</span>
+                <span className="flex items-center col-span-2 text-secondary-dark text-sm font-medium text-center dark:text-white">DKK {totalPrice(invoice.itemList)}</span>
+                <div className="flex items-center justify-center col-span-2">
                     <InvoiceStatus invoiceStatus={invoice.status} />
                 </div>
-                <FontAwesomeIcon className="text-primary-regular hidden sm:block" icon={faAngleRight} />
+                <div className="hidden col-span-1 sm:flex items-center justify-end">
+                    <FontAwesomeIcon className="text-primary-regular" icon={faAngleRight} />
+                </div>
             </div>
           </a>
         </Link>
