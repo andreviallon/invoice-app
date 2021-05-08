998af76af580600c5ddcfc80a6df3bdcfef9d9d1
Andre Viallon <viallon.andre@gmail.com>
2021-05-08 14:05:37 +0200

    Fix go back button allignment

--- components/GoBack.tsx
+++ components/GoBack.tsx
@@ -8,7 +8,7 @@ const GoBack = () => {
         <Link href={'/'}>
             <a>
                 <div className="flex items-center cursor-pointer hover:text-secondary-dark dark:text-white dark:hover:text-secondary-regular">
-                    <FontAwesomeIcon className="text-primary-regular mr-2 mb-2" icon={faAngleLeft} />
+                    <FontAwesomeIcon className="text-primary-regular mr-2 mb-1" icon={faAngleLeft} />
                     <p className="font-bold text-sm">Go Back</p>
                 </div>
             </a>
