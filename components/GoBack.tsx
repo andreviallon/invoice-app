921737bdba95f7a4e3c53495559859fd6e2116d6
Andre Viallon <viallon.andre@gmail.com>
2021-05-10 19:18:12 +0200

    Remove unused import

--- components/GoBack.tsx
+++ components/GoBack.tsx
@@ -1,13 +1,12 @@
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import React from 'react';
 
 const GoBack = () => {
     return (
         <Link href={'/'}>
             <a>
-                <div className="flex items-center cursor-pointer hover:text-secondary-dark dark:text-white dark:hover:text-secondary-regular">
+                <div className="inline-flex items-center cursor-pointer hover:text-secondary-dark dark:text-white dark:hover:text-secondary-regular">
                     <FontAwesomeIcon className="text-primary-regular mr-2 mb-1" icon={faAngleLeft} />
                     <p className="font-bold text-sm">Go Back</p>
                 </div>
