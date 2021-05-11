73c93120145842b71ea90154a1b3cadcf5510edb
Andre Viallon <viallon.andre@gmail.com>
2021-05-11 18:27:16 +0200

    Navigate back to root when clikcing the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ThemeEnum } from '../models/Theme';
 import { useTheme } from 'next-themes';
 import { useLoaded } from 'customHooks/useLoaded';
+import Link from 'next/link';
 
 const Navbar = () => {
     const loaded = useLoaded();
@@ -22,12 +23,16 @@ const Navbar = () => {
     return (
         <div className={`${navbarStyles} navbar`}>
             <div className="cursor-pointer w-16 sm:w-full">
-                <div className="hidden sm:block">
-                    <Image src="/logo_with_background.svg" className="rounded-r-2xl" alt="Logo" layout="fixed" width={85} height={85} />
-                </div>
-                <div className="block sm:hidden">
-                    <Image src="/logo_with_background.svg" className="rounded-r-2xl" alt="Logo" layout="fixed" width={64} height={64} />
-                </div>
+                <Link href={'/'}>
+                    <a>
+                    <div className="hidden sm:block">
+                        <Image src="/logo_with_background.svg" className="rounded-r-2xl" alt="Logo" layout="fixed" width={85} height={85} />
+                    </div>
+                    <div className="block sm:hidden">
+                        <Image src="/logo_with_background.svg" className="rounded-r-2xl" alt="Logo" layout="fixed" width={64} height={64} />
+                    </div>
+                    </a>
+                </Link>
             </div>
             <div className="flex justify-end items-center w-full sm:flex-col flex-row pb-0 pr-4 sm:pb-4 sm:pr-0">
                 <div className="m-8">
